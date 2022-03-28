ae8a8b2e650324dd47a9b38463b02596233475d4
Gero Lindner <gero.lindner@protonmail.com>
2022-03-28 20:36:25 +0200

    Initial commit

---
+++ src/main/java/com/github/awesomegero/lootbox/LootboxPlugin.java
@@ -0,0 +1,15 @@
+package com.github.awesomegero.lootbox;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LootboxPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
